a0fdaeb1be2a0bf9556f4a7e97fc04fca2983370
Phuc Anh <75134436+anhhp0@users.noreply.github.com>
2024-01-03 10:32:52 +0700

    update

--- src/components/siteFooter/svg/MainInfomations.tsx
+++ src/components/siteFooter/svg/MainInfomations.tsx
@@ -91,14 +91,3 @@ interface MainInfomationsProps {
   className: string;
 }
 
-/**
- * This component was generated from Figma with FireJet.
- * Learn more at https://www.firejet.io
- *
- * README:
- * The output code may look slightly different when copied to your codebase. To fix this:
- * 1. Include the necessary fonts. The required fonts are imported from public/index.html
- * 2. Include the global styles. They can be found in App.css
- *
- * Note: Step 2 is not required for tailwind.css output
- */
