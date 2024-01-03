6b8fb35cc163ece2f800fe5eab3f0741a36463cd
Phuc Anh <75134436+anhhp0@users.noreply.github.com>
2024-01-03 17:19:13 +0700

    fix css

--- src/components/siteHeader/page.tsx
+++ src/components/siteHeader/page.tsx
@@ -12,25 +12,46 @@ export default function SiteHeader({ className = "" }: siteHeaderProps) {
         <div className="contact">Contact </div>
       </button>
       <div className="main-menu">
-        <button className="blog">Blog </button>
-        <button className="gallery">Gallery </button>
-        <button className="service">Service </button>
-        <button className="about">About </button>
-        <button className="home">
-          <svg
-            className="plus-icon"
-            width="7"
-            height="7"
-            viewBox="0 0 7 7"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <line x1="3.5" y1="2.18557e-08" x2="3.5" y2="7" stroke="#41487F" />
-            <line x1="7" y1="3.5" x2="-4.37114e-08" y2="3.5" stroke="#41487F" />
-          </svg>
-
-          <div className="home2">Home </div>
-        </button>
+        <div className="blog">
+          <a href="/">Blog</a>{" "}
+        </div>
+        <div className="gallery">
+          <a href="/">Gallery</a>{" "}
+        </div>
+        <div className="service">
+          <a href="/">Service</a>{" "}
+        </div>
+        <div className="about">
+          <a href="/">About</a>{" "}
+        </div>
+        <a href="/">
+          <div className="home">
+            <svg
+              className="plus-icon"
+              width="7"
+              height="7"
+              viewBox="0 0 7 7"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <line
+                x1="3.5"
+                y1="2.18557e-08"
+                x2="3.5"
+                y2="7"
+                stroke="#41487F"
+              />
+              <line
+                x1="7"
+                y1="3.5"
+                x2="-4.37114e-08"
+                y2="3.5"
+                stroke="#41487F"
+              />
+            </svg>
+            <div className="home2">Home </div>
+          </div>
+        </a>
       </div>
       <div className="main-logo">
         <div className="ellipse-bg"></div>
